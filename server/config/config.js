c7458b8a26407d16d09860a9cc09449626fe257c
user.name <rm.mahady@gmail.com>
2021-02-21 16:12:22 +0300

    x

---
+++ server/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+    port : process.env.PORT || 5555,
+    // mongoURI : process.env.MONGO_URI || "mongodb://localhost:27017/sharingapp-pl55",
+    jwtSecret : process.env.JWT_SECRET || "jwt152secrethmac256",
+    smtp : {
+        user : '......',
+        pass : '......'
+    }
+}
+export default config;
