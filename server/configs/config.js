381e6a3c5566e686855f90de919dd7cd6bcd1b85
user.name <rm.mahady@gmail.com>
2021-02-23 20:09:52 +0300

    Ongoing dev

---
+++ server/configs/config.js
@@ -0,0 +1,11 @@
+const config = {
+    port : process.env.PORT || 5555,
+    // mongoURI : process.env.MONGO_URI || "mongodb://localhost:27017/sharingapp-pl55",
+    mongoURI : process.env.MONGO_URI || "mongodb+srv://mahady:mahady1906@cluster0.tasds.mongodb.net/mahady?retryWrites=true&w=majority",
+    jwtSecret : process.env.JWT_SECRET || "jwt152secrethmac256",
+    smtp : {
+        user : 'rm.mahady@gmail.com',
+        pass : 'mahady1906-janoary2021'
+    }
+}
+export default config;
