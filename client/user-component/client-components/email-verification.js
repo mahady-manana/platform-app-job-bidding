c5b16030345dd602f4fc4b3b9fc70dbd37f0f96b
Dazy007 <dazysylvana@gmail.com>
2021-02-23 17:31:22 +0100

    Ongoing dev

---
+++ client/user-component/client-components/email-verification.js
@@ -0,0 +1,8 @@
+import React from "react";
+export const EmailVerification=()=>{
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- client/user-component/client-components/pre.signup.client.js
+++ client/user-component/client-components/pre.signup.client.js
@@ -1,6 +1,24 @@
 import React,{useState} from 'react';
 import {Input} from './utility';
+
 const ClientPreSignup = () => {
+    const[values, setValues]= useState({
+        firstname:'',
+        lastname:'',
+        email:'',
+        password:'',
+    })
+
+  const handleChange=name=>event=>{
+       event.preventDefault()
+       const val = event.target.value;
+       setValues({...values,[name]:val})
+
+  }
+  const handleSubmit=event=>{
+        event.preventDefault()
+        console.log(values)
+  }
 return (
 <>
 <div className='pre-signup'>
@@ -9,27 +27,36 @@ return (
         <div className='inner-section'>
             <div className='container'>
                 <div className="form-container">
-                    <form>
+                    <form onSubmit={handleSubmit}>
                        <Input type="text"
-                              Name="firstname"
+                              name="firstname"
                               labelName="First Name"
                               placeholder="Enter firstname"
+                              value={values.firstname}
+                              onChange={handleChange('firstname')}
+
                        />
 
                         <Input type="text"
                                 name="last name"
                                 labelName="Laste Name"
                                 placeholder="Enter lastname"
+                                value={values.lastname}
+                                onChange={handleChange('lastname')}
                         />
                         <Input type="email"
                                 name="email"
                                 labelName="Email"
                                 placeholder="Enter Email"
+                                value={values.email}
+                                onChange={handleChange('email')}
                         />
                         <Input type="password"
                                 name="password"
                                 labelName="Password"
                                 placeholder="Enter password"
+                               value={values.password}
+                               onChange={handleChange('password')}
                         />
                         <button type="submit" className="btn default-1">Save and next</button>
                     </form>
