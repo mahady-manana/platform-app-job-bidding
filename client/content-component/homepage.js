4b98b04bcc7b1650a4aba3c50c91dfb7cddb3a16
user.name <rm.mahady@gmail.com>
2021-04-03 08:50:48 +0300

    Delete some unnecessary content

--- client/content-component/homepage.js
+++ client/content-component/homepage.js
@@ -12,72 +12,8 @@ return (
                     <div className='text-center'>
                         <h1 className='mc-3'>Go Inside</h1>
                         <h2 className='text-white'>Client-Talent Partnership</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elitn laborum! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo et distinctio omnis, 
-                        perspiciatis labore sequi nemo dolorum molestias commodi cumque 
-                        pariatur praesentium esse cupiditate eos nisi id deleniti in laborum!</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elitn laborum!</p>
                         <button className='btn default-1'>Find Talented Freelancer</button>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section className='section'>
-            <div className='inner-section'>
-                <div className='container'>
-                    <h3 className='text-center mc-3'>Top 5 Freelancer at Go-Inside</h3>
-                    <div className='divider dotted-sm'></div>
-                    <div className='slide-freelancer-home'>
-                        <SlideFreelancer/>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section className="section">
-            <div className='inner-section'>
-                <div className='container'>
-                    <div className='row grid-home-1'>
-                        <div className='col-md-3 item'>
-                            <div className='inner-col'>
-                                <h3>Lorem ipsum</h3>
-                                <p>Explicabo et distinctio omnis, 
-                                perspiciatis labore sequi nemo dolorum molestias commodi</p>
-                            </div>
-                        </div>
-                        <div className='col-md-3 item'>
-                            <div className='inner-col'>
-                                <h3>Distinctio omnis</h3>
-                                <p>Explicabo et distinctio omnis, 
-                                perspiciatis labore sequi nemo dolorum molestias commodi</p>
-                            </div>
-                        </div>
-                        <div className='col-md-3 item'>
-                            <div className='inner-col'>
-                                <h3>Labore sequi</h3>
-                                <p>Explicabo et distinctio omnis, 
-                                perspiciatis labore sequi nemo dolorum molestias commodi</p>
-                            </div>                            
-                        </div>
-                        <div className='col-md-3 item'>
-                            <div className='inner-col'>
-                                <h3>Dolorum molestias</h3>
-                                <p>Explicabo et distinctio omnis, 
-                                perspiciatis labore sequi nemo dolorum molestias commodi</p>
-                            </div>                            
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section className='section'>
-            <div className='inner-section'>
-                <div className='container'>
-                    <div className='text-center'>
-                        <h2>World Class Talent</h2>
-                        <h4>Find out the most talented Freelancer in the World</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo et distinctio omnis, 
-                            perspiciatis labore sequi nemo dolorum molestias commodi cumque 
-                            pariatur praesentium esse cupiditate eos nisi id deleniti in laborum! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo et distinctio omnis, 
-                            perspiciatis labore sequi nemo dolorum molestias commodi cumque 
-                            pariatur praesentium esse cupiditate eos nisi id deleniti in laborum!</p>
                     </div>
                 </div>
             </div>
@@ -105,65 +41,6 @@ return (
                     </div>
                     <div className='image-contaier image image6'>
                         <img src='/images/laptop.png' alt='Go inside'/>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section className='section'>
-            <div className='inner-section mbgm'>
-                <div className='container'>
-                    <div className='row'>
-                        <div className='col-md-6'>
-                            <div className='inner-col text-white'>
-                                <h2>Labore sequi nemo dolorum molestias</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo et distinctio omnis, 
-                                perspiciatis labore sequi nemo dolorum molestias commodi cumque 
-                                pariatur praesentium esse cupiditate eos nisi id deleniti in laborum! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo et distinctio omnis, 
-                                perspiciatis labore sequi nemo dolorum molestias commodi cumque 
-                                pariatur praesentium esse cupiditate eos nisi id deleniti in laborum!</p>
-                            </div>
-                        </div>
-                        <div className='col-md-6'>
-                            <div className='inner-col'>
-                                <div className='images-container'>
-                                    <img src='/images/fashion.jpg' alt='fashion'/>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section className='section'>
-            <div className='inner-section'>
-                <div className='container'>
-                    <div className='text'>
-                        <div className='col'>
-                            <div className='inner-col text-center'>
-                                <h2>Labore sequi nemo</h2>
-                                <h3>Lorem ipsum dolor sit amet</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo et distinctio omnis, 
-                                perspiciatis labore sequi nemo dolorum molestias commodi cumque 
-                                pariatur praesentium esse cupiditate eos nisi id deleniti in laborum! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo et distinctio omnis, 
-                                perspiciatis labore sequi nemo dolorum molestias commodi cumque 
-                                pariatur praesentium esse cupiditate eos nisi id deleniti in laborum!</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section className='section'>
-            <div className='inner-section'>
-                <div className='container'>
-                    <div className='text'>
-                        <div className='col'>
-                            <div className='inner-col text-center'>
-                                <h2>Testimonials</h2>
-                                <h3>What they said...</h3>
-                                <p>Testimonials here</p>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
